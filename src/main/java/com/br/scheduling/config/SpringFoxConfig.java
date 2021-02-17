b2a5a7dd12e63caf9eab1f3188833ba6b651913c
unknown <mpegrucci1@gmail.com>
2021-02-17 18:17:19 -0300

    Add ordering by methods in swagger

--- src/main/java/com/br/scheduling/config/SpringFoxConfig.java
+++ src/main/java/com/br/scheduling/config/SpringFoxConfig.java
@@ -1,14 +1,20 @@
 package com.br.scheduling.config;
 
+import com.google.common.collect.Ordering;
+import org.hibernate.sql.ordering.antlr.OrderingSpecification;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Operation;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -17,6 +23,7 @@ import java.util.ArrayList;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -40,5 +47,13 @@ public class SpringFoxConfig {
         );
 
         return apiInfo;
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder
+                .builder()
+                .operationsSorter(OperationsSorter.METHOD)
+                .build();
     }
 }
